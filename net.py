c37c5ad211b4d194bf70a7592c5838760e085b80
NMCxyz <cuong220103@gmail.com>
2024-02-15 22:10:25 +0700

    add regression rnn

--- net.py
+++ net.py
@@ -127,7 +127,6 @@ class ClassifyRNN(nn.Module):
         self.cls = nn.Linear(hidden_size_2, output_size)
 
     def forward(self, x):
-        batch_size = x.size(0)
         out, _ = self.rnn1(x)
         out, _ = self.rnn2(out)
         out = self.dropout(out)
@@ -150,35 +149,7 @@ class ClassifyGRU(nn.Module):
         out = out.contiguous().view(-1, out.size(-1))  
         out = self.cls(out)
         return out
-    
-class RegressionNN(nn.Module):
-    def __init__(
-            self,
-            input_size,
-            hidden_size_1,
-            hidden_size_2,
-            output_size,
-            dropout
-        ):
-        super(RegressionNN, self).__init__()
 
-        self.fc1 = nn.Linear(input_size, hidden_size_1)
-        self.relu1 = nn.ReLU()
-        self.dropout1 = nn.Dropout(dropout)
-        self.fc2 = nn.Linear(hidden_size_1, hidden_size_2)
-        self.relu2 = nn.ReLU()
-        self.dropout2 = nn.Dropout(dropout)
-        self.fc3 = nn.Linear(hidden_size_2, output_size)  
-
-    def forward(self, x):
-        x = self.fc1(x)
-        x = self.relu1(x)
-        x = self.dropout1(x)
-        x = self.fc2(x)
-        x = self.relu2(x)
-        x = self.dropout2(x)
-        x = self.fc3(x)  
-        return x
     
 class RegressionLSTM(nn.Module):
     
@@ -205,3 +176,20 @@ class RegressionLSTM(nn.Module):
         reg_out = self.reg(out[:, -1, :]).flatten()
 
         return reg_out
+    
+class RegressionRNN(nn.Module):
+    def __init__(self, input_size, hidden_size_1, hidden_size_2, output_size, dropout):
+        super(RegressionRNN, self).__init__()
+        self.rnn1 = nn.RNN(input_size, hidden_size_1, batch_first=True)
+        self.rnn2 = nn.RNN(hidden_size_1, hidden_size_2, batch_first=True)
+        self.dropout = nn.Dropout(dropout)
+        self.reg = nn.Linear(hidden_size_2, 1)
+
+    def forward(self, x):
+        out, _ = self.rnn1(x)
+        out, _ = self.rnn2(out)
+        out = self.dropout(out)
+        out = out[:, -1, :]
+        reg_out = self.reg(out).squeeze()  
+        return reg_out
+
--- train_regression.py
+++ train_regression.py
@@ -6,7 +6,7 @@ from dataset import get_data_mtl
 from dataset import RegressionDataset
 from trainer.regression_trainer import RegressionTrainer
 from net import (
-    RegressionLSTM,
+    RegressionRNN,
     reg_loss_fn,
     reg_metric
 )
@@ -57,7 +57,7 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     
     # Initialize the PyTorch model
-    model = RegressionLSTM(
+    model = RegressionRNN(
         input_size=args.input_dim,
         hidden_size_1=args.n_hidden_1,
         hidden_size_2=args.n_hidden_2,
