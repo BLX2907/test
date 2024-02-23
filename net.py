cd7c4cb54037faf9442e01ebd004d021bafdd480
NMCxyz <cuong220103@gmail.com>
2024-02-23 16:50:16 +0700

    ok

--- net.py
+++ net.py
@@ -245,10 +245,10 @@ class SELayer(nn.Module):
 
 # Thử 1: Ko ổn lắm
 # Thử 2: num_lstm_out=64, lstm_drop_p=0.5 ==> đớ
-# Thử cuối: 
+# Thử cuối: giảm lstm drop out
 class MLSTMfcn(nn.Module):
     def __init__(self, *, num_classes, max_seq_len, num_features,
-                    num_lstm_out=128, num_lstm_layers=1, 
+                    num_lstm_out=64, num_lstm_layers=1, 
                     conv1_nf=16, conv2_nf=32, conv3_nf=16,
                     lstm_drop_p=0.4, fc_drop_p=0.3):
         super(MLSTMfcn, self).__init__()
